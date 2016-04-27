a00cdc44ad040ebd6fb123b2a0a3d06ab675107b
zain-jr <zainjrtech21@gmail.com>
2016-04-27 11:40:12 +0500

    firest commit

--- markup/js/jquery.main.js
+++ markup/js/jquery.main.js
@@ -1,9 +1,96 @@
+ function previewAddPropertyImg(file, target)
+ {
+	previewFile(file, target);
+	$(file).closest('li').find('.picture-name').removeClass('disableInput');
+	$(file).closest('li').find('.image-holder').css({
+		 'opacity':'1',
+		 'z-index':'5'
+	});
+	$(file).closest('.file-uploader').addClass('after-none');
+	$(file).closest('li').find('.picture-name').focus();
+ }
+ 
+ $(document).on('click', '.propertyDocumentCloseBtn', function(){
+	 $(this).closest('.image-holder').css({
+		 'opacity':'0',
+		 'z-index':'-1'
+	 });
+	 $(this).closest('.file-uploader').removeClass('after-none');
+	 $(this).closest('li').find('.picture-name').addClass('disableInput');
+ });
+ 
+function previewFile(file, target) {
+  var preview = document.querySelector(target);
+  var file    = file.files[0];
+  var reader  = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = "";
+  }
+}
+
+ $(function() {
+	 if ($(".addPropertyFormContianer")[0]){
+		handleAddPropertyFormScrolling();
+		$(document).on("scroll", onScroll);
+	 }
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.scrollAddPropertyNavLink').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos+50 && refElement.position().top + refElement.height() > scrollPos) {
+			$(currLink).closest('li').siblings().removeClass('activeAddPropertyNavLink');
+			$(currLink).closest('li').addClass('activeAddPropertyNavLink');
+        }
+        else{
+            currLink.closest('li').removeClass('activeAddPropertyNavLink');
+        }
+    });
+}
+
+function handleAddPropertyFormScrolling()
+{
+	$('.scrollAddPropertyNavLink').click(function() {
+			$(this).closest('li').siblings().removeClass('activeAddPropertyNavLink');
+			$(this).closest('li').addClass('activeAddPropertyNavLink');
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			console.log(target.offset());
+			if (target.length) {
+				$('html, body').animate({
+				scrollTop: target.offset().top
+				}, 500);
+				return false;
+			}
+		}
+	});	
+}
+
+$(window).scroll(function(){
+	if ($(".addPropertyFormContianer")[0]){
+		if ($(this).scrollTop() >= $('#header').height()) {
+			$('.addPropertyFormContianer').addClass('fixed-position');
+		} else {
+			$('.addPropertyFormContianer').removeClass('fixed-position');
+		}
+	}
+});
+
+
 $(".searchable-select").select2({
 	placeholder: "Select",
   allowClear: true
 });
-
-
 
 // page init
 jQuery(function(){
