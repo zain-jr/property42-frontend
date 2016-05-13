aa3aa86a3d55562c9ec720cb4cdb887a523342ed
zain-jr <zainjrtech21@gmail.com>
2016-05-13 06:09:15 -0700

    structure

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,23 +1,7 @@
-$(function() {
-     if ($(".addPropertyFormContainer")[0]){
-        handleAddPropertyFormScrolling();
-		$(document).on("scroll", onScroll);
-     }
-});
-
-$(window).scroll(function(){
-	if ($(".addPropertyFormContainer")[0]){
-		if ($(this).scrollTop() >= $('#header').height()) {
-			$('.addPropertyFormContainer').addClass('fixed-position');
-		} else {
-			$('.addPropertyFormContainer').removeClass('fixed-position');
-		}
-	}
-});
-
 // page init
 jQuery(function(){
 	initTabs();
+	initAnchors();
 });
 
 // content tabs init
@@ -27,5 +11,30 @@ function initTabs() {
 		effect: 'fade',
 		animSpeed:200,
 		switchTime:1000
+	});
+}
+// smooth anchor links
+function initAnchors() {
+	/* global SmoothScroll */
+
+	// simple case:
+	new SmoothScroll({
+		anchorLinks: '.smooth-scroll'
+	});
+
+	// common case:
+	new SmoothScroll({
+		extraOffset: $('.anchor-nav').height() || 0,
+		anchorLinks: '.anchor-nav a, .side-nav a',
+		activeClasses: 'link',
+		wheelBehavior: 'ignore',
+		animDuration: 800
+	});
+
+	// custom case:
+	new SmoothScroll({
+		anchorLinks: '.inner-links a',
+		container: '.inner-container',
+		activeClasses: 'parent'
 	});
 }
